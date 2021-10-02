75195bddeb88acc5df38c898281b968d93c17df4
Tomás Ellis <tomasitoellis@gmail.com>
2021-10-01 22:28:30 -0300

    feat: changed contract to actually receive and emit wave events

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,24 +6,46 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-    address[] coolPeople;
+
+    struct Wave {
+        address waver; 
+        string message; 
+        uint256 timestamp; 
+    }
+
+
+    Wave[] coolPeopleWaves;
 
     constructor() {
         console.log("Beeg wave");
     }
 
-    function wave() public {
-        totalWaves += 1;
-        coolPeople.push(msg.sender);
-        
-        for (uint i=0; i < coolPeople.length; i++) {
-            if(i == coolPeople.length - 1){
-                console.log("Welcome to the cool people club, %s!", coolPeople[i]);
-            } else {
-                console.log("Sup' %s", coolPeople[i]);
-            }
-        }
+    event NewWave(address indexed from, uint256 timestamp, string message);
 
+    
+
+
+    function wave(string memory _message) public {
+        // First increase the total
+        totalWaves += 1;
+
+        // Add a new wave
+        coolPeopleWaves.push(Wave(msg.sender, _message, block.timestamp));
+        
+        // Tell everyone we've just been waved at
+        emit NewWave(msg.sender, block.timestamp, _message);
+        
+        // for (uint i=0; i < coolPeople.length; i++) {
+        //     if(i == coolPeople.length - 1){
+        //         console.log("Welcome to the cool people club, %s!", coolPeople[i]);
+        //     } else {
+        //         console.log("Sup' %s", coolPeople[i]);
+        //     }
+        // }
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return coolPeopleWaves;
     }
 
     function getTotalWaves() public view returns (uint256) {
