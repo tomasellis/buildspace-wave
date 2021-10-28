e8392f18f687163a69bfe6444e3e4ad562d4b2fe
Tomás Ellis <tomasitoellis@gmail.com>
2021-10-27 23:40:55 -0300

    chore: comments on contract functionality

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -35,15 +35,19 @@ contract WavePortal {
         // Tell everyone we've just been waved at
         emit NewWave(msg.sender, block.timestamp, _message);
        
+        // Set eth to send user after wave 
         uint256 prizeAmount = 0.0001 ether;
         
+        // Check if the contract has enough eth to even think about sending
         require(
             prizeAmount <= address(this).balance,
             "Trying to withdraw more money than the contract has."
         );
 
+        // If cool, proceed
         (bool success, ) = (msg.sender).call{value: prizeAmount}("");
         
+        // If didn't suceed, tell the blockchain
         require(success, "Failed to withdraw money from contract.");
     }
 
